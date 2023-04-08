5ae1e15e3ba436764a4c29907d60d2e58397cb6a
luisf <luisf@DESKTOP-P0D3UQL>
2023-04-07 22:02:28 -0700

    homework2

--- src/d2hw2/Hw2.java
+++ src/d2hw2/Hw2.java
@@ -4,7 +4,10 @@ public class Hw2 {
 	public static void main(String[] args) {
 		
 }
-	public void practice4() {
-		System.out.println("Homework practice 4");
+	public void practice2() {
+		System.out.println("Homework practice 2");
+	}
+	public void stash2() {
+		System.out.println("stashing practice 2");
 	}
 }
